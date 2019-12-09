2748320c0eede44939c51ab5a937ee6dcb6196e7
BeastHQ <Bestia@users.noreply.github.com>
2019-12-09 21:41:28 +0500

    Homework is finished

---
+++ homework#2_main.c
@@ -0,0 +1,19 @@
+#include "homework#2_header.h"
+int main()
+{
+  //1st task
+  printf("%d -> %d\n", N, dec_to_bin(N));
+
+  //2nd task
+  //b
+  printf("%d\n", pow_rec(2, 6));
+
+  //c
+  printf("%d\n", pow_rec_second(2, 9));
+
+  //2.5 is on the pic homework#2_2.5.jpg
+
+  //3rd task
+  printf("%d\n", program(3, 20));
+  return 0;
+}
